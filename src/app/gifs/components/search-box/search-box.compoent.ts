e302b2261618f7ecf3978cf9f2c902e82de96ccb
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-19 17:52:10 -0600

    GifsService

--- src/app/gifs/components/search-box/search-box.compoent.ts
+++ src/app/gifs/components/search-box/search-box.compoent.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -18,11 +19,15 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag});
+
+    this.gifsService.searchTag(newTag);
+
+    // Limpia la caja de texto
+    this.tagInput.nativeElement.value= '';
   }
 
 
