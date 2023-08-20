ab7eb49c9eee4fd10662a9e69a5aa0195928fc83
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-19 19:03:47 -0600

    Peticion Http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
@@ -6,8 +7,11 @@ export class GifsService {
   // Propiedad que almacena todo lo que se va buscando
   private _tagsHistory: string[] = [];
 
+  private apiKey: string = 'TFKzUK9fxf7nU2NIpOaXppngr20o00d0';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
 
   get tagsHistory() {
     return this._tagsHistory;
@@ -28,11 +32,21 @@ export class GifsService {
   // Busca los valores del tag que las personas le estan indicando
   searchTag(tag:string): void {
     if(tag.length === 0) return;
-
     this.organizeHistory(tag);
 
-    // Limita el arreglo a 10
-    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    const params = new HttpParams()
+      .set('api-key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+      .subscribe(resp => {
+        console.log(resp);
+      })
+
+
+
+
   }
 
 }
